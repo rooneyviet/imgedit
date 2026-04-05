d02c716b1862330d79d48e41ef0eab829df77fe1
Nguyen Viet Quan <nguyenvietquan0304@gmail.com>
2026-04-05 19:34:21 +0900

    Rewrite CI/CD for TanStack Start Nitro on VPS

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,9 +6,15 @@ BRANCH="${BRANCH:-master}"
 
 cd "$APP_DIR"
 
-git fetch origin "$BRANCH"
-git checkout "$BRANCH"
-git pull --ff-only origin "$BRANCH"
+if [[ -n "${GITHUB_TOKEN:-}" ]]; then
+  git -c http.extraheader="Authorization: Bearer ${GITHUB_TOKEN}" fetch origin "$BRANCH"
+  git checkout "$BRANCH"
+  git -c http.extraheader="Authorization: Bearer ${GITHUB_TOKEN}" pull --ff-only origin "$BRANCH"
+else
+  git fetch origin "$BRANCH"
+  git checkout "$BRANCH"
+  git pull --ff-only origin "$BRANCH"
+fi
 
 docker compose -f docker-compose.prod.yml up -d --build --remove-orphans
 docker image prune -f
